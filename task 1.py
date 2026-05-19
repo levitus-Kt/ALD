c2553667600501b1623c21fe270c7a5ffcbb1224
levitus-Kt <fetdenal@ya.ru>
2026-05-19 19:34:32 +0300

    Realize function

--- task 1.py
+++ task 1.py
@@ -1,20 +1,27 @@
 def process_grades(records: list[str]) -> dict:
     valid_count = 0
     total_sum = 0
-    passed_students = []
+    passed = []
     skipped = 0
 
     for record in records:
         parts = record.split(":")
+        # Проверка на двоеточие
         if len(parts) != 2:
+            skipped += 1
+            continue
+
+        if len(parts[0]) < 1 or len(parts[1]) < 1:
             skipped += 1
             continue
 
         surname, grade_str = parts
         grade_str = grade_str.strip()
 
+        # Проверка, что оценка - число
         try:
             grade = int(grade_str)
+            # Проверка на диапазон оценек
             if not (0 <= grade <= 100):
                 raise ValueError("Неправильная оценка")
 
@@ -22,19 +29,38 @@ def process_grades(records: list[str]) -> dict:
             total_sum += grade
 
             if grade >= 60:
-                passed_students.append(surname)
+                passed.append(surname)
         except (ValueError, TypeError):
             skipped += 1
 
     average = round(total_sum / valid_count, 1) if valid_count > 0 else 0.0
-    passed_students.sort()
+    passed = list(set(passed))
+    passed.sort()
 
     return {
         "valid_count": valid_count,
         "average": average,
-        "passed": passed_students,
+        "passed": passed,
         "skipped": skipped,
     }
+
+
+def read_data():
+    try:
+        # Получаем путь к файлу
+        file_path = input("Введите путь к файлу с данными: ")
+    except Exception as ex:
+        emergency_exit(f"Неизвестная ошибка: {ex}, попробуйте еще раз")
+    else:
+        try:
+            with open(file_path, "r", encoding="utf-8") as file:
+                # Читаем данные
+                grades = file.readlines()
+        except FileNotFoundError as ex:
+            emergency_exit(f"Файл не найден: {ex}")
+        except Exception as ex:
+            emergency_exit(f"Неизвестная ошибка: {ex}")
+    return grades
 
 
 def emergency_exit(message):
@@ -43,27 +69,11 @@ def emergency_exit(message):
 
 
 if __name__ == "__main__":
-    records = []
+    grades = read_data()
 
-    try:
-        # Получаем путь к файлу
-        file_path = input("Введите полный путь к файлу с данными: ")
-    except Exception as ex:
-        emergency_exit(f"Неизвестная ошибка: {ex}, попробуйте еще раз")
-    else:
-        try:
-            with open(file_path, "r", encoding="utf-8") as file:
-                # Читаем данные
-                records = file.readlines()
-                print(records)
-        except FileNotFoundError as ex:
-            emergency_exit(f"Файл не найден: {ex}")
-        except Exception as ex:
-            emergency_exit(f"Неизвестная ошибка: {ex}")
-
-    if len(records) == 0:
+    if len(grades) == 0:
         emergency_exit("В файле нет данных")
-    if type(records) is not list:
+    if type(grades) is not list:
         emergency_exit("Неверный формат данных")
 
-    process_grades(records)
+    print(process_grades(grades))
